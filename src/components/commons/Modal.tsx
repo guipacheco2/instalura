e84ee117583a3e4ac4f5b2636e808ea94bdd72a6
Guilherme Pacheco <guipacheco2@gmail.com>
2021-02-24 21:44:31 -0300

    Modulo 02 Aula 02

--- src/components/commons/Modal.tsx
+++ src/components/commons/Modal.tsx
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react'
+import { motion } from 'framer-motion'
+import React from 'react'
 import styled, { css } from 'styled-components'
 
 interface StyledModalProps {
@@ -9,15 +10,16 @@ const StyledModal = styled.div<StyledModalProps>`
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  background: rgba(0, 0, 0, 0.1);
+  background: rgba(0, 0, 0, 0.9);
   position: fixed;
-  z-index: 2;
+  z-index: 100;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
   margin: auto;
   overflow: scroll;
+  transition: 0.3s;
   ${({ isOpen }) => {
     if (isOpen) {
       return css`
@@ -38,16 +40,10 @@ interface ModalProps extends StyledModalProps {
 }
 
 export function Modal({ isOpen, onClose, children }: ModalProps): JSX.Element {
-  const innerElementRef = useRef<HTMLDivElement>(null)
-
   function handleClickOutsideInnerElement(
     event: React.MouseEvent<HTMLDivElement, MouseEvent>,
   ) {
-    if (!innerElementRef.current) throw new Error('Unable to use ref.')
-
-    const isSafeArea = innerElementRef.current.contains(
-      event.target as HTMLElement,
-    )
+    const isSafeArea = event.currentTarget !== event.target
 
     if (!isSafeArea) {
       onClose()
@@ -55,8 +51,16 @@ export function Modal({ isOpen, onClose, children }: ModalProps): JSX.Element {
   }
 
   return (
-    <StyledModal isOpen={isOpen} onClick={handleClickOutsideInnerElement}>
-      <div ref={innerElementRef}>{children}</div>
+    <StyledModal isOpen={isOpen}>
+      <motion.div
+        onClick={handleClickOutsideInnerElement}
+        variants={{ open: { x: 0 }, closed: { x: '-100%' } }}
+        animate={isOpen ? 'open' : 'closed'}
+        transition={{ duration: 0.5 }}
+        style={{ display: 'flex', flex: 1 }}
+      >
+        {children}
+      </motion.div>
     </StyledModal>
   )
 }
