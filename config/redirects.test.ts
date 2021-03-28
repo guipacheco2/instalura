8a8c0069811e75cf73ceb0080bd1fb60ac27b6b9
Guilherme Pacheco <guipacheco2@gmail.com>
2021-03-28 20:06:19 -0300

    test(jest): add function tests with jest

---
+++ config/redirects.test.ts
@@ -0,0 +1,9 @@
+/// <reference types="jest" />
+
+import redirects from './redirects'
+
+describe('config/redirects', () => {
+  test('renders all current redirects', () => {
+    expect(redirects).toMatchSnapshot()
+  })
+})
